f98071c3ffc83afc6ce8618ea487a4f295ff395e
paulwilcox <t78t78@gmail.com>
2019-01-05 08:34:16 -0700

    Finalizing node_scripts structure.

---
+++ node_scripts/versionBump.js
@@ -0,0 +1,12 @@
+let pjVersion = require('./package.json').version;
+let fs = require('fs');
+
+fs.copyFile(
+    './dist/lish.next.js', 
+    `./dist/lish.${pjVersion}.js`, 
+    err => {
+        if (err) throw err;
+        console.log(`created lish.${pjVersion}.js from lish.next.js`);
+    }
+)
+
