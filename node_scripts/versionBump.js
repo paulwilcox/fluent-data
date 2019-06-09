42dc15415658c8751c933abb1862564651f972b9
paulwilcox <t78t78@gmail.com>
2019-06-09 11:23:23 -0700

    VersionBump.js server/client logic.

--- node_scripts/versionBump.js
+++ node_scripts/versionBump.js
@@ -2,11 +2,19 @@ let pjVersion = require('../package.json').version;
 let fs = require('fs');
 
 fs.copyFile(
-    './dist/oneQuery.next.js', 
-    `./dist/oneQuery.${pjVersion}.js`, 
+    './dist/oneQuery.client.next.js', 
+    `./dist/oneQuery.client.${pjVersion}.js`, 
     err => {
         if (err) throw err;
-        console.log(`created oneQuery.${pjVersion}.js from oneQuery.next.js`);
+        console.log(`created oneQuery.client.${pjVersion}.js from oneQuery.client.next.js`);
     }
-)
+);
 
+fs.copyFile(
+    './dist/oneQuery.server.next.js', 
+    `./dist/oneQuery.server.${pjVersion}.js`, 
+    err => {
+        if (err) throw err;
+        console.log(`created oneQuery.server.${pjVersion}.js from oneQuery.server.next.js`);
+    }
+);
