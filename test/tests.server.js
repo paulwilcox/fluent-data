93a4658c91ddec55698efc2ad1ffb64e54730d33
paulwilcox <t78t78@gmail.com>
2019-10-05 15:28:33 -0700

    tests.js utilizes FluentDB.test

--- test/tests.server.js
+++ test/tests.server.js
@@ -15,12 +15,34 @@ var tests = async (seriesName, createFDB) =>
 
         createFDB()
             .filter(o => o.customer == 2)
-            .test(
-                'filter', 
-                o => o,
-                data =>  
-                    data.filter(x => x.customer == 2).length > 0 
-                    && data.filter(x => x.customer != 2).length == 0
+            .test('filter', o => o, data => 
+                data.filter(x => x.customer == 2).length > 0 && 
+                data.filter(x => x.customer != 2).length == 0
+            ),
+
+        createFDB()
+            .map(o => ({
+                customer: o.customer,
+                rating: o.rating,
+                flag: o.rating < 10 ? 'bad' : o.rating < 50 ? 'okay' : 'good'
+            }))
+            .test('map', o => o, data =>
+                Object.keys(data[0]).includes('customer') && 
+                !Object.keys(data[0]).includes('id')
+            ),
+            
+        createFDB()
+            .join((o,p) => o.product == p.id)
+            .test('join', o => o, data => 
+                Object.keys(data[0]).includes('price')
+            ),
+
+        createFDB()
+            .merge('upsert', c => c.id, pc => pc.id)
+            .merge('delete', c => c.id, s => s.id)
+            .test('merge', c => c, data =>  
+                data.find(row => row.id == 2).fullName == 'Johnathan Doe' && 
+                data.filter(row => row.id == 4 || row.id == 5).length == 0
             )
             
     ])
@@ -31,50 +53,6 @@ var tests = async (seriesName, createFDB) =>
             row.name = row.testName;
         }
         return res;
-    }); 
-
-/*
-    // map
-
-        data = await createFDB()
-            .map(o => ({
-                customer: o.customer,
-                rating: o.rating,
-                flag: o.rating < 10 ? 'bad' : o.rating < 50 ? 'okay' : 'good'
-            }))
-            .execute(o => o);
-
-        await reg('map',
-               Object.keys(data[0]).includes('customer') 
-            && !Object.keys(data[0]).includes('id')
-        );
-
-    // join 
-
-        data = await createFDB()
-            .join((o,p) => o.product == p.id)
-            .execute(o => o);
-
-        await reg('join', 
-            Object.keys(data[0]).includes('price')
-        );
-
-    // merge
-
-        if (seriesName == 'idbHybridLeft')
-        data = await createFDB()
-            .merge('upsert', c => c.id, pc => pc.id)
-            .merge('delete', c => c.id, s => s.id)
-            .execute();  
-            
-        await reg('merge', 
-               data.find(row => row.id == 2).fullName == 'Johnathan Doe'
-            && data.filter(row => row.id == 4 || row.id == 5).length == 0
-        );
-
-    // terminate
-
-        return results;
-*/
+    });
 
 module.exports = tests;
