3a21ded2b96fd8b80a42accd5854d288c64d9c24
paulwilcox <t78t78@gmail.com>
2019-10-27 19:58:56 -0700

    sampleData.idb.js rebuilt (no longer uses IDB lib)

--- dist/sampleData.idb.js
+++ dist/sampleData.idb.js
@@ -7,342 +7,6 @@
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-function createCommonjsModule(fn, module) {
-	return module = { exports: {} }, fn(module, module.exports), module.exports;
-}
-
-var idb = createCommonjsModule(function (module) {
-
-(function() {
-  function toArray(arr) {
-    return Array.prototype.slice.call(arr);
-  }
-
-  function promisifyRequest(request) {
-    return new Promise(function(resolve, reject) {
-      request.onsuccess = function() {
-        resolve(request.result);
-      };
-
-      request.onerror = function() {
-        reject(request.error);
-      };
-    });
-  }
-
-  function promisifyRequestCall(obj, method, args) {
-    var request;
-    var p = new Promise(function(resolve, reject) {
-      request = obj[method].apply(obj, args);
-      promisifyRequest(request).then(resolve, reject);
-    });
-
-    p.request = request;
-    return p;
-  }
-
-  function promisifyCursorRequestCall(obj, method, args) {
-    var p = promisifyRequestCall(obj, method, args);
-    return p.then(function(value) {
-      if (!value) return;
-      return new Cursor(value, p.request);
-    });
-  }
-
-  function proxyProperties(ProxyClass, targetProp, properties) {
-    properties.forEach(function(prop) {
-      Object.defineProperty(ProxyClass.prototype, prop, {
-        get: function() {
-          return this[targetProp][prop];
-        },
-        set: function(val) {
-          this[targetProp][prop] = val;
-        }
-      });
-    });
-  }
-
-  function proxyRequestMethods(ProxyClass, targetProp, Constructor, properties) {
-    properties.forEach(function(prop) {
-      if (!(prop in Constructor.prototype)) return;
-      ProxyClass.prototype[prop] = function() {
-        return promisifyRequestCall(this[targetProp], prop, arguments);
-      };
-    });
-  }
-
-  function proxyMethods(ProxyClass, targetProp, Constructor, properties) {
-    properties.forEach(function(prop) {
-      if (!(prop in Constructor.prototype)) return;
-      ProxyClass.prototype[prop] = function() {
-        return this[targetProp][prop].apply(this[targetProp], arguments);
-      };
-    });
-  }
-
-  function proxyCursorRequestMethods(ProxyClass, targetProp, Constructor, properties) {
-    properties.forEach(function(prop) {
-      if (!(prop in Constructor.prototype)) return;
-      ProxyClass.prototype[prop] = function() {
-        return promisifyCursorRequestCall(this[targetProp], prop, arguments);
-      };
-    });
-  }
-
-  function Index(index) {
-    this._index = index;
-  }
-
-  proxyProperties(Index, '_index', [
-    'name',
-    'keyPath',
-    'multiEntry',
-    'unique'
-  ]);
-
-  proxyRequestMethods(Index, '_index', IDBIndex, [
-    'get',
-    'getKey',
-    'getAll',
-    'getAllKeys',
-    'count'
-  ]);
-
-  proxyCursorRequestMethods(Index, '_index', IDBIndex, [
-    'openCursor',
-    'openKeyCursor'
-  ]);
-
-  function Cursor(cursor, request) {
-    this._cursor = cursor;
-    this._request = request;
-  }
-
-  proxyProperties(Cursor, '_cursor', [
-    'direction',
-    'key',
-    'primaryKey',
-    'value'
-  ]);
-
-  proxyRequestMethods(Cursor, '_cursor', IDBCursor, [
-    'update',
-    'delete'
-  ]);
-
-  // proxy 'next' methods
-  ['advance', 'continue', 'continuePrimaryKey'].forEach(function(methodName) {
-    if (!(methodName in IDBCursor.prototype)) return;
-    Cursor.prototype[methodName] = function() {
-      var cursor = this;
-      var args = arguments;
-      return Promise.resolve().then(function() {
-        cursor._cursor[methodName].apply(cursor._cursor, args);
-        return promisifyRequest(cursor._request).then(function(value) {
-          if (!value) return;
-          return new Cursor(value, cursor._request);
-        });
-      });
-    };
-  });
-
-  function ObjectStore(store) {
-    this._store = store;
-  }
-
-  ObjectStore.prototype.createIndex = function() {
-    return new Index(this._store.createIndex.apply(this._store, arguments));
-  };
-
-  ObjectStore.prototype.index = function() {
-    return new Index(this._store.index.apply(this._store, arguments));
-  };
-
-  proxyProperties(ObjectStore, '_store', [
-    'name',
-    'keyPath',
-    'indexNames',
-    'autoIncrement'
-  ]);
-
-  proxyRequestMethods(ObjectStore, '_store', IDBObjectStore, [
-    'put',
-    'add',
-    'delete',
-    'clear',
-    'get',
-    'getAll',
-    'getKey',
-    'getAllKeys',
-    'count'
-  ]);
-
-  proxyCursorRequestMethods(ObjectStore, '_store', IDBObjectStore, [
-    'openCursor',
-    'openKeyCursor'
-  ]);
-
-  proxyMethods(ObjectStore, '_store', IDBObjectStore, [
-    'deleteIndex'
-  ]);
-
-  function Transaction(idbTransaction) {
-    this._tx = idbTransaction;
-    this.complete = new Promise(function(resolve, reject) {
-      idbTransaction.oncomplete = function() {
-        resolve();
-      };
-      idbTransaction.onerror = function() {
-        reject(idbTransaction.error);
-      };
-      idbTransaction.onabort = function() {
-        reject(idbTransaction.error);
-      };
-    });
-  }
-
-  Transaction.prototype.objectStore = function() {
-    return new ObjectStore(this._tx.objectStore.apply(this._tx, arguments));
-  };
-
-  proxyProperties(Transaction, '_tx', [
-    'objectStoreNames',
-    'mode'
-  ]);
-
-  proxyMethods(Transaction, '_tx', IDBTransaction, [
-    'abort'
-  ]);
-
-  function UpgradeDB(db, oldVersion, transaction) {
-    this._db = db;
-    this.oldVersion = oldVersion;
-    this.transaction = new Transaction(transaction);
-  }
-
-  UpgradeDB.prototype.createObjectStore = function() {
-    return new ObjectStore(this._db.createObjectStore.apply(this._db, arguments));
-  };
-
-  proxyProperties(UpgradeDB, '_db', [
-    'name',
-    'version',
-    'objectStoreNames'
-  ]);
-
-  proxyMethods(UpgradeDB, '_db', IDBDatabase, [
-    'deleteObjectStore',
-    'close'
-  ]);
-
-  function DB(db) {
-    this._db = db;
-  }
-
-  DB.prototype.transaction = function() {
-    return new Transaction(this._db.transaction.apply(this._db, arguments));
-  };
-
-  proxyProperties(DB, '_db', [
-    'name',
-    'version',
-    'objectStoreNames'
-  ]);
-
-  proxyMethods(DB, '_db', IDBDatabase, [
-    'close'
-  ]);
-
-  // Add cursor iterators
-  // TODO: remove this once browsers do the right thing with promises
-  ['openCursor', 'openKeyCursor'].forEach(function(funcName) {
-    [ObjectStore, Index].forEach(function(Constructor) {
-      // Don't create iterateKeyCursor if openKeyCursor doesn't exist.
-      if (!(funcName in Constructor.prototype)) return;
-
-      Constructor.prototype[funcName.replace('open', 'iterate')] = function() {
-        var args = toArray(arguments);
-        var callback = args[args.length - 1];
-        var nativeObject = this._store || this._index;
-        var request = nativeObject[funcName].apply(nativeObject, args.slice(0, -1));
-        request.onsuccess = function() {
-          callback(request.result);
-        };
-      };
-    });
-  });
-
-  // polyfill getAll
-  [Index, ObjectStore].forEach(function(Constructor) {
-    if (Constructor.prototype.getAll) return;
-    Constructor.prototype.getAll = function(query, count) {
-      var instance = this;
-      var items = [];
-
-      return new Promise(function(resolve) {
-        instance.iterateCursor(query, function(cursor) {
-          if (!cursor) {
-            resolve(items);
-            return;
-          }
-          items.push(cursor.value);
-
-          if (count !== undefined && items.length == count) {
-            resolve(items);
-            return;
-          }
-          cursor.continue();
-        });
-      });
-    };
-  });
-
-  var exp = {
-    open: function(name, version, upgradeCallback) {
-      var p = promisifyRequestCall(indexedDB, 'open', [name, version]);
-      var request = p.request;
-
-      if (request) {
-        request.onupgradeneeded = function(event) {
-          if (upgradeCallback) {
-            upgradeCallback(new UpgradeDB(request.result, event.oldVersion, request.transaction));
-          }
-        };
-      }
-
-      return p.then(function(db) {
-        return new DB(db);
-      });
-    },
-    delete: function(name) {
-      return promisifyRequestCall(indexedDB, 'deleteDatabase', [name]);
-    }
-  };
-
-  {
-    module.exports = exp;
-    module.exports.default = module.exports;
-  }
-}());
-});
-
-var node = createCommonjsModule(function (module) {
-if (typeof indexedDB != 'undefined') {
-  module.exports = idb;
-}
-else {
-  module.exports = {
-    open: function () {
-      return Promise.reject('IDB requires a browser environment');
-    },
-    delete: function () {
-      return Promise.reject('IDB requires a browser environment');
-    }
-  };
-}
-});
-var node_1 = node.open;
-
 var sample = {
 
     products: [
@@ -406,12 +70,116 @@ var sample = {
 
 };
 
-async function sampleData_idb (dbName, reset, keyPaths) { 
+async function sampleData_idb (
+    
+    dbName, 
+
+    // omit to do no resets, 
+    // pass true to reset from sampleData,
+    // pass an {object} of key:data's to reset to that database
+    // pass an [{key,keyPath,data}] to reset to that database with keypaths
+    // pass a 'key' to reset only that key from sampleData          
+    reset = false,
+
+    // set to true to delete any dataset not represented in reset
+    deleteWhenNotInReset = false 
+    
+) {
+    
+    let fullReset = reset === true && deleteWhenNotInReset;
+
+    if (fullReset)
+        await indexedDB.deleteDatabase(dbName);
+
+    let version =
+        fullReset ? 1 
+        : reset ? await getDbVersion(dbName) + 1
+        : undefined;
+
+    return new Promise((res,rej) => {
+        let dbOpenRequest = indexedDB.open(dbName, version);
+        dbOpenRequest.onsuccess = event => res(event.target.result);
+        dbOpenRequest.onupgradeneeded = 
+            event => upgrade(event.target.result, reset, deleteWhenNotInReset);
+    });
+
+}
+
+async function getDbVersion(dbName) {
+    return new Promise((res,rej) => {
+        let dbOpenRequest = indexedDB.open(dbName);
+        dbOpenRequest.onsuccess = event => {
+            let db = event.target.result;
+            let v = db.version;
+            db.close();
+            res(v);
+        };
+    });
+}
+
+async function upgrade (db, reset, deleteWhenNotInReset) { 
+
+    let isObject = typeof reset === 'object' && Object.keys(reset).length > 0;
+
+    let datasets = 
+        reset === true ? sample
+        : isObject ? reset
+        : typeof reset === 'string' ? { [reset]: sample[reset] }
+        : Array.isArray(reset) ? reset.reduce((a,b) => Object.assign(a, {[b.key]: b.data}), {})
+        : null;
+        
+    let keyPaths = 
+        reset === true ? Object.keys(sample).map(key => ({ [key]: 'id' }))
+        : isObject ? Object.keys(datasets).reduce((a,b) => Object.assign(a, {[b.key]: 'id' }), {})
+        : typeof reset === 'string' ? { [reset]: { [key]: 'id' } }
+        : Array.isArray(reset) ? reset.reduce((a,b) => Object.assign(a, {[b.key]: b.keyPath}), {})
+        : null;
+
+    let deleteKeys = 
+        deleteWhenNotInReset 
+        ? [...db.objectStoreNames]
+        : Object.keys(datasets);
+        
+    for (let key of deleteKeys) { 
+        if ([...db.objectStoreNames].indexOf(key) == -1)
+            continue;
+        console.log(`deleting ${key}`);
+        await db.deleteObjectStore(key);
+    }
+
+    for (let key of Object.keys(datasets)) { 
+
+        console.log(`creating ${key}`);
+
+        // if the first row of the store contains the expected key, 
+        // then no autoincrement, otherwise yes.
+        let store = await db.createObjectStore(key, {
+            keyPath: keyPaths[key],
+            autoIncrement: datasets[key].length > 0 && !Object.keys(datasets[key][0]).includes(keyPaths[key])
+        });
+
+        for (let row of datasets[key]) 
+            store.put(row);
+
+    }
+
+}
+
+
+/*
+import * as idb from 'idb';
+import sample from './sampleData.client.js';
+
+// TODO: try to create better reset behavior that matches
+// what now exists in sampleData.mongo.js.  Then make sure
+// it doesn't break runTests.js.  Then write tests for 
+// FluentDB.mergeExternal() on IDB and Mongo as targets.
+export default async function (dbName, reset, keyPaths) { 
 
     if (reset)
         await indexedDB.deleteDatabase(dbName);
 
-    return node_1(dbName, 1, async db => { 
+    return idb.open(dbName, 1, async db => { 
 
         let data = Object.keys(reset).length > 0 ? reset : sample;
         keyPaths = keyPaths || Object.keys(data).map(key => ({ [key]: 'id' }));
@@ -438,5 +206,7 @@ async function sampleData_idb (dbName, reset, keyPaths) {
     });
 
 }
+
+*/
 
 export default sampleData_idb;
--- src/samples/sampleData.idb.js
+++ src/samples/sampleData.idb.js
@@ -1,3 +1,102 @@
+import sample from './sampleData.client.js';
+
+export default async function (
+    
+    dbName, 
+
+    // omit to do no resets, 
+    // pass true to reset from sampleData,
+    // pass an {object} of key:data's to reset to that database
+    // pass an [{key,keyPath,data}] to reset to that database with keypaths
+    // pass a 'key' to reset only that key from sampleData          
+    reset = false,
+
+    // set to true to delete any dataset not represented in reset
+    deleteWhenNotInReset = false 
+    
+) {
+    
+    let fullReset = reset === true && deleteWhenNotInReset;
+
+    if (fullReset)
+        await indexedDB.deleteDatabase(dbName);
+
+    let version =
+        fullReset ? 1 
+        : reset ? await getDbVersion(dbName) + 1
+        : undefined;
+
+    return new Promise((res,rej) => {
+        let dbOpenRequest = indexedDB.open(dbName, version);
+        dbOpenRequest.onsuccess = event => res(event.target.result);
+        dbOpenRequest.onupgradeneeded = 
+            event => upgrade(event.target.result, reset, deleteWhenNotInReset);
+    });
+
+}
+
+async function getDbVersion(dbName) {
+    return new Promise((res,rej) => {
+        let dbOpenRequest = indexedDB.open(dbName);
+        dbOpenRequest.onsuccess = event => {
+            let db = event.target.result;
+            let v = db.version;
+            db.close();
+            res(v);
+        };
+    });
+}
+
+async function upgrade (db, reset, deleteWhenNotInReset) { 
+
+    let isObject = typeof reset === 'object' && Object.keys(reset).length > 0;
+
+    let datasets = 
+        reset === true ? sample
+        : isObject ? reset
+        : typeof reset === 'string' ? { [reset]: sample[reset] }
+        : Array.isArray(reset) ? reset.reduce((a,b) => Object.assign(a, {[b.key]: b.data}), {})
+        : null;
+        
+    let keyPaths = 
+        reset === true ? Object.keys(sample).map(key => ({ [key]: 'id' }))
+        : isObject ? Object.keys(datasets).reduce((a,b) => Object.assign(a, {[b.key]: 'id' }), {})
+        : typeof reset === 'string' ? { [reset]: { [key]: 'id' } }
+        : Array.isArray(reset) ? reset.reduce((a,b) => Object.assign(a, {[b.key]: b.keyPath}), {})
+        : null;
+
+    let deleteKeys = 
+        deleteWhenNotInReset 
+        ? [...db.objectStoreNames]
+        : Object.keys(datasets);
+        
+    for (let key of deleteKeys) { 
+        if ([...db.objectStoreNames].indexOf(key) == -1)
+            continue;
+        console.log(`deleting ${key}`);
+        await db.deleteObjectStore(key);
+    }
+
+    for (let key of Object.keys(datasets)) { 
+
+        console.log(`creating ${key}`);
+
+        // if the first row of the store contains the expected key, 
+        // then no autoincrement, otherwise yes.
+        let store = await db.createObjectStore(key, {
+            keyPath: keyPaths[key],
+            autoIncrement: datasets[key].length > 0 && !Object.keys(datasets[key][0]).includes(keyPaths[key])
+        });
+
+        for (let row of datasets[key]) 
+            store.put(row);
+
+    }
+
+}
+
+
+/*
 import * as idb from 'idb';
 import sample from './sampleData.client.js';
 
@@ -37,3 +136,5 @@ export default async function (dbName, reset, keyPaths) {
     });
 
 }
+
+*/
