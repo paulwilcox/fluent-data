f60b1f43d427d03e244c432061a269ee1d9d2366
paulwilcox <t78t78@gmail.com>
2019-11-02 10:17:17 -0700

    runtests.js code better organized.

--- test/runTests.js
+++ test/runTests.js
@@ -3,63 +3,56 @@ let server = require('../server.js');
 let getServerResults = require('./runServerTests.js');
 require('console.table');
 
-// TODO: Separate server code from puppeteer code.  
 // TODO: Allow output for when all tests pass.  Use this
 // in a "tests" script in package.json
 
 let seriToRun = process.argv[2];
 let testsToRun = process.argv[3];
+let results = [];
 
 let clientUrl = 'http://127.0.0.1:8081/runClientTests'
 if (seriToRun) clientUrl += `&seriToRun='${seriToRun}'`;
 if (testsToRun) clientUrl += `&testsToRun='${testsToRun}'`; 
 clientUrl = clientUrl.replace('&', '?'); // replaces only first '&', which is actually what we want
 
-(async () => {
+async function getClientResults (headless = true) {
 
-    let headless = true;
     let browser = await puppeteer.launch({headless: headless});
     let page = await browser.newPage();
-    let closed = false;
-    let results = [];
-
-    let close = async () => {
-        if (closed)
-            return;
-        await browser.close();
-        console.log('headless browser closed');
-        await new Promise((res,rej) => res(server.close()));
-        console.log('server closed');
-        closed = true;
-    }
 
     page.on('console', async msg => {
-        let logs = msg.text().split(' ');
-        if (logs[0] == 'done:client.tests') {
-            await close();
-            let serverResults = await getServerResults(seriToRun, testsToRun);
-            results.push(...serverResults);             
-            console.table(results);
-            console.log();
-            process.exit(0);
-        }
-        else if (logs[0] == 'test')
-            results.push({
-                test_name: logs[1], 
-                status: logs[2] === 'true' ? 'pass' : 'fail'
-            });  
-        else
-            console.log(msg.text());
-    });
 
-    page.on('pageerror', err => {
-        console.log(err);
-        close();
+        if (msg.text() == 'done:client.tests') {
+            await browser.close();
+            console.log('headless browser closed');    
+            return;
+        }
+        
+        if (!msg.text().startsWith('test')) {
+            console.log(msg.text());
+            return;
+        }
+
+        let parts = msg.text().split(' ');
+        results.push({
+            test_name: parts[1], 
+            status: parts[2] === 'true' ? 'pass' : 'fail'
+        });  
+        
     });
 
     await page.goto(clientUrl);
 
+}
+
+(async () => {
+
+    getClientResults();
+    let serverResults = await getServerResults(seriToRun, testsToRun);
+    results.push(...serverResults);    
+    server.close(() => console.log('server closed'));
+    console.log();
+    console.table(results); 
+    process.exit(0);    
+
 })();
-
-
-
