91b22cf5d72df4fb842c1ef27491a47cc490c951
paulwilcox <t78t78@gmail.com>
2019-11-11 13:28:48 -0700

    working with wiki workflow (not done)

---
+++ node_scripts/gitCommitWiki.cmd
@@ -0,0 +1,4 @@
+(
+    cd .\wiki
+    git commit -a
+)
---
+++ node_scripts/gitPublishWiki.cmd
@@ -0,0 +1,5 @@
+( 
+    cd .\wiki
+    git commit -a -m "pre-publish commit"
+    git push https://github.com/paulwilcox/FluentDB.wiki.git master
+)
