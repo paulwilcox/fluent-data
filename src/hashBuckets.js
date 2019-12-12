6312c1cd7aed60f6e3e096c64666d084d382ba2a
paulwilcox <t78t78@gmail.com>
2019-12-12 11:03:11 -0700

    HashBuckets looks more like buckles (untested).

--- src/hashBuckets.js
+++ src/hashBuckets.js
@@ -1,13 +1,15 @@
 import * as g from "./general.js";
 
-
-export default class {
+export default class extends Map {
     
     constructor (
-        hashKeySelector
+        hashKeySelector,
+        stringify = true
     ) {
-        this.mapper = new Map();
-        this.hashKeySelector = hashKeySelector;
+        super();
+        this.hashKeySelector = stringify 
+            ? item => g.stringifyObject(hashKeySelector(item)) 
+            : hashKeySelector;
     }
 
     addItems(items) {
@@ -17,64 +19,55 @@ export default class {
     }
 
     addItem(item) {
-
-        let objectKey = this.hashKeySelector(item);
-        let stringKey = g.stringifyObject(objectKey);
-
-        if (!this.mapper.has(stringKey)) 
-            this.mapper.set(stringKey, [item]);
-        else 
-            this.mapper.get(stringKey).push(item);
-
+        let key = this.hashKeySelector(item);
+        if(!this.has(key))
+            this.set(key, []);
+        this.get(key).push(item)
         return this;
-
     }
 
     getBucket(
         objectToHash, 
         hashKeySelector,
-        remove = false
+        stringify
     ) {
-
-        let objectKey = hashKeySelector(objectToHash);
-        let stringKey = g.stringifyObject(objectKey);
-
-        let value = this.mapper.get(stringKey);
-
-        if (remove) 
-            this.mapper.delete(stringKey);
-
-        return value;
-
-    }
-
-    getBucketFirstItem (
-        objectToHash,
-        hashKeySelector,
-        remove = false
-    ) {
-
-        let bucket = 
-            this.getBucket(
-                objectToHash,
-                hashKeySelector,
-                remove
-            );
-
-        if (!bucket || bucket.length == 0)
-            return null;
-
-        return bucket[0];
-
-    }
-
-    getKeys() {
-        return Array.from(this.mapper.keys());
+        let key = stringify
+            ? g.stringifyObject(hashKeySelector(objectToHash))
+            : hashKeySelector(objectToHash);
+        return this.mapper.get(key);
     }
 
     getBuckets() {
         return Array.from(this.mapper.values());
     }
 
+    * crossMap(incomingRows, mapper) {
+        for (let incomingRow of incomingRows)
+            for(let outputRow of crossMapRow(incomingRow, mapper))
+                yield outputRow;
+    }
+
+    * crossMapRow(incomingRow, hashKeySelector, stringify, mapper) {
+                
+        let existingRows = this.getBucket(incomingRow, hashKeySelector, stringify);
+
+        if (existingRows === undefined)
+            existingRows = [undefined];
+
+        for(let existingRow of existingRows) {
+
+            let mapped = mapper(existingRow, incomingRow);
+
+            if (Array.isArray(mapped)) 
+                for (let entry of mapped) 
+                    if (entry !== undefined)
+                        yield entry;
+            else if (mapped !== undefined)
+                yield mapped;
+
+        }
+
+    }
+    
 }
 
