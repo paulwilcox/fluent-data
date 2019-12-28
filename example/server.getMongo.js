b6204d831711e9d96b6f36e6ac09a03b0fff782d
paulwilcox <t78t78@gmail.com>
2019-12-28 14:09:36 -0700

    connectorMongo.merge working.
    Well, not fully tested, but works in one test case.

--- example/server.getMongo.js
+++ example/server.getMongo.js
@@ -1,15 +1,18 @@
 let sample = require('../node_modules/sampledb/dist/SampleDB.server.js');
+let sampleMongo = require('../node_modules/sampledb/dist/SampleDB.mongo.js');
 let $$ = require('../dist/FluentDB.server.js');
 
 module.exports = async function () {
 
-    return $$({
+    await sampleMongo('mongodb://localhost:27017/SampleDB', true, true);
+
+    return await $$({
             c: $$.mongo('customers', 'mongodb://localhost:27017/SampleDB'),
             o: sample.orders
         })
-        //.merge((c,o) => c.id == o.customer, 'both left')
+        .merge((c,o) => c.id == o.customer, 'both left')
         .import(c => c)
         .execute(c => c)
         .then(res => JSON.stringify(res));
-
+        
 }
--- src/connectorMongo.js
+++ src/connectorMongo.js
@@ -58,4 +58,75 @@ export default class extends connector {
 
     }    
 
+    merge (
+        incoming, 
+        matchingLogic, 
+        mapFunc, 
+        distinct = false
+    ) {
+
+        let keyFuncs = parser.pairEqualitiesToObjectSelectors(matchingLogic);
+        let targetKeyFunc = keyFuncs.leftFunc;
+        let sourceKeyFunc = keyFuncs.rightFunc;    
+        let processedTargets = new hashBuckets(targetKeyFunc, true);
+        let mapper = normalizeMapper(mapFunc);
+
+        let incomingBuckets = 
+            new hashBuckets(sourceKeyFunc, distinct)
+            .addItems(incoming);
+
+        this.client = this.client.then(async client => {
+
+            let db = client.db();
+            let col = await db.collection(this.collectionName);
+
+            await col.find().forEach(record => {
+
+                // If user wants distinct rows in the target, then
+                // track if such a row has already been processed.
+                // If so, delete future rows in the target.  If not,
+                // just record that it has now been processed.
+                if (distinct) {  
+                    let processedTarget = processedTargets.getBucket(record, targetKeyFunc);
+                    if (processedTarget) {
+                        col.removeOne( { _id: record._id });
+                        return;
+                    }
+                    processedTargets.addItem(record);
+                }
+
+                // Finds the bucket of incoming rows matching the 
+                // target and 'crossMaps' them.  Returns a generator. 
+                let outputGenerator = incomingBuckets.crossMapRow(
+                    record, 
+                    targetKeyFunc,
+                    mapper
+                );
+
+                // For the first match, delete or update. based on
+                // whether there's a match or not.
+                let outputYield = outputGenerator.next();
+                (outputYield.done) 
+                    ? col.removeOne({ _id: record._id })
+                    : col.replaceOne({ _id: record._id }, outputYield.value);
+
+                // For additional matches, insert them to the collection.
+                outputYield = outputGenerator.next();
+                while (outputYield.done === false) {
+                    delete outputYield.value._id; // needs a new id
+                    col.insertOne(outputYield.value);
+                    outputYield = outputGenerator.next();
+                }
+
+            })
+  
+            return client;
+
+        })
+
+        return this;
+
+    }
+    
+
 } 
