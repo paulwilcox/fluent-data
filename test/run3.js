aa7a3e1f380dfb13c6da7520ba133ee4850bf35f
paulwilcox <t78t78@gmail.com>
2019-12-30 15:15:47 -0700

    Further work on run3.js
    The client side testing structure.  See todo note for next step.

--- test/run3.js
+++ test/run3.js
@@ -2,58 +2,67 @@ let fs = require('fs');
 let http = require('http');
 let puppeteer = require('puppeteer');
 
+// TODO: Write file contents into script in a way that
+// '#results' captures the return value.  Then use the
+// output.
+
 let server = http.createServer(async (request, response) => {
 
-    if (!request.endsWith('.js')) {
+    if (!request.url.endsWith('.js')) {
         response.writeHead(204);
         response.end();
         return;
     }
 
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end(`console.log('here')`, 'utf-8');
+    response.end(`
+
+        <body>
+        <script>
+            let div = document.createElement('div');
+            div.id = 'results';       
+            document.body.appendChild(div); 
+        </script>
+        </body> 
+
+    `, 'utf-8');
 
 })
-.listen(8081);
+.listen(8082);
 
 async function makeClientRequest (fileName) {
 
-    let clientUrl = `http://127.0.0.1:8081/${fileName}`
-    let browser = await puppeteer.launch({headless: headless});
+    let browser = await puppeteer.launch({headless: true});
     let page = await browser.newPage();
+    let pageErrored = false;
 
     page.on('pageerror', async err => {
-
-        console.log('makeClientRequest page error');
-
+        console.log({pageError: err});
+        pageErrored = true;
         await browser.close();
-        console.log(
-            'Error from browser when running tests: ', 
-            err.name, 
-            err.message, 
-            err.stack
-        );    
-        return;
-
     });
 
-    await page.goto(clientUrl);
+    await page.goto(`http://127.0.0.1:8082/${fileName}`);
+
+    if (pageErrored) {
+        return;
+    }
+
     await page.waitForSelector('#results');
     
     let clientResults = await page.evaluate(() => 
         document.querySelector('#results').innerHTML
     );
 
-    for (let clientResult of clientResults.split(';')) {
-        if (clientResult == '')
-            continue;
-        let parts = clientResult.split(':');
-        results.push({
-            test_name: parts[0],
-            status: parts[1] == 'true' ? 'pass' : 'fail'
-        });
-    }
-
     await browser.close();
 
+    return clientResults;
+
 }
+
+makeClientRequest('filterTest.js')
+.then(x => {
+    console.log({x});
+})
+.finally(() => server.close());
+
