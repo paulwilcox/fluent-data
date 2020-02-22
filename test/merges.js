747adb0f8c66a85d5f0a414ff36ff9188e045800
paulwilcox <t78t78@gmail.com>
2020-02-22 16:11:19 -0700

    test/merges.js: now accounts for all merge types.

--- test/merges.js
+++ test/merges.js
@@ -1,30 +1,83 @@
 async function test () {
 
-    let results = await merge('both both');
-
-    return results.length == 5 
-        && confirm(results, 'c', 'Confucius', 'change');
-
-}
-
-async function merge(mapper) {
-
     let data = await sample('philosophites, mathematicians');
+    let testIt = (mapper, expIds, expTop) => 
+        new mergeTester(data, mapper, expIds, expTop);
 
-    let results = await
-        $$({
-            p: data.philosophites,
-            m: data.mathematicians
-        })
-        .merge((p,m) => p.id == m.id, mapper)
-        .execute(p => p);
+    testIt('both both', 'a,b,c', 'Bentham'); // full join
+    testIt('both left', 'a,b', 'Bentham'); // left join
+    testIt('both right', 'b,c', 'Bentham'); // right join
+    testIt('both null', 'b', 'Bentham'); // inner join 
 
-    return results;
+    testIt('right both', 'a,b,c', 'bijection'); // upsert
+    testIt('right left', 'a,b', 'bijection'); // update
+    testIt('right right', 'b,c', 'bijection'); // replace
+    testIt('right null', 'b', 'bijection'); // uplete
+
+    testIt('left both', 'a,b,c', 'Bentham'); // insert
+    testIt('left left', 'a,b', 'Bentham'); // preserve
+    testIt('left right', 'b,c', 'Bentham'); // reverse update
+    testIt('left null', 'b', 'Bentham'); // exists
+
+    testIt('null both', 'a,c', undefined); // dual not exists
+    testIt('null left', 'a', undefined); // not exists
+    testIt('null right', 'c', undefined); // reverse not exists
+    testIt('null null', '', undefined); // clear
+
+    return true;
 
 }
 
-function confirm(results, rowId, subject, term) {
-    let row = results.find(row => row.id == rowId);
-    return row.subject == subject 
-        && row.term == term;
+class mergeTester {
+
+    constructor(data, mapper, expectedIds, expectedBTopic) {
+        this.mapper = mapper;
+        this.mapperStr = (typeof mapper == 'string') 
+            ? mapper 
+            : 'custom mapper';
+        this.results = this._merge(data, mapper);
+        this._checkIds(expectedIds);
+        this._checkTopic('b', expectedBTopic)
+    }
+
+    _checkIds(expectedIds) {
+        expectedIds = expectedIds == '' ? [] : expectedIds.split(',');
+        let resultIds = this.results.map(r => r.id);
+        let rExcess = resultIds.some(r => !expectedIds.includes(r));
+        let eExcess = expectedIds.some(e => !resultIds.includes(e));
+        if(rExcess || eExcess)
+            throw `Expected id's for '${this.mapperStr}' ` +
+              `are '${expectedIds.join(',')}'; but actual ` + 
+              `is '${resultIds.join(',')}'.`;
+    }
+
+    _checkTopic(rowId, topic) {
+
+        let row = this.results.find(row => row.id == rowId);
+        let isEqual = (a,b) => a == b || (!a && !b);
+
+        if (!topic && !row)
+            return;
+
+        if(!isEqual(row.topic, topic))
+            throw `Expected topic for '${this.mapperStr}' ` +
+                `at row.id == '${rowId}' is '${topic}' ` +
+                `but '${row.topic}' was found.`
+
+    }
+
+    _merge(data, mapper) {
+
+        let results = 
+            $$({
+                p: data.philosophites,
+                m: data.mathematicians
+            })
+            .merge((p,m) => p.id == m.id, mapper)
+            .execute(p => p);
+
+        return results;
+
+    }
+
 }
