e773f5ca7df34539cdb210167c17175e55324d21
paulwilcox <t78t78@gmail.com>
2020-04-26 12:09:50 -0700

    database.get(): get fixed for string key.

--- src/database.js
+++ src/database.js
@@ -61,7 +61,7 @@ export default class {
     // of the calling FluentDB.
     get(funcOrKey) {
         if (g.isString(funcOrKey))
-            return this.datasets[funcOrKey].data;
+            return this.datasets[funcOrKey].get();
         let key = parser.parameters(funcOrKey)[0];
         return this
             ._callOnDs('get', funcOrKey)
