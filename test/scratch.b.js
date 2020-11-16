8ae53a3bed87dcc1dd16c90067f4206f0941b2e8
paulwilcox <t78t78@gmail.com>
2020-11-15 22:22:05 -0700

    scratch: trivial

--- test/scratch.b.js
+++ test/scratch.b.js
@@ -9,24 +9,23 @@ async function test () {
         [1, -1,  0]
     ]);
 
-    let Acol1 = A.clone().get(null, 0);
-    let e1 = $$.matrix.identity(A.data.length).get(null, 0);
-    let v1 = Acol1.subtract(e1.multiply(Math.sign(Acol1.data[0]) * Acol1.norm())); 
-    let v1v1t = v1.clone().multiply(v1.clone().transpose());
+    let col0 = A.clone().get(null, 0);
+    let e = $$.matrix.identity(A.data.length).get(null, 0);
+    let v = col0.subtract(e.multiply(Math.sign(col0.data[0]) * col0.norm())); 
+    let vvt = v.clone().multiply(v.clone().transpose());
 
-    let H = v1.clone().transpose().multiply(v1).data[0];
+    let H = v.clone().transpose().multiply(v).data[0];
     H = 2 / H;
-    H = v1v1t.clone().multiply(H);
+    H = vvt.clone().multiply(H);
     H = $$.matrix.identity(H.data[0].length).subtract(H);
 
     // Same as H * A, but presumably more performant
-    let Anext = v1v1t.clone();
+    let Anext = vvt.clone();
     Anext = Anext.multiply(0.5).multiply(A);
     Anext = A.clone().subtract(Anext);
 
     console.log({
         A: A.data,
-        v1: v1.data,
         H: H.data,
         Anext: Anext.data,  
     })
