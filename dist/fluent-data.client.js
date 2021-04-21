97b8f890c24daadb5b55d4f1cc4c4228b74826bd
paulwilcox <t78t78@gmail.com>
2021-04-20 22:50:02 -0700

    preMerge commit

--- dist/fluent-data.client.js
+++ dist/fluent-data.client.js
@@ -508,92 +508,6 @@ function pochLogged (q, n) {
     return prod;
 }
 
-class hashBuckets extends Map {
-    
-    constructor (
-        hashKeySelector,
-        distinct = false
-    ) {
-        super();
-        this.distinct = distinct;
-        this.hashKeySelector = hashKeySelector;
-    }  
- 
-    addItems(items) {
-        for(let item of items)
-            this.addItem(item);
-        return this;
-    }
-
-    addItem(item) {
-
-        let key = this.hashKeySelector(item);
-        if (!isString(key))
-            key = stringifyObject(key);
-
-        if (this.distinct) {
-            this.set(key, [item]);
-            return this;
-        }
-
-        if(!this.has(key))
-            this.set(key, []);
-
-        this.get(key).push(item);
-
-        return this;
-
-    }
-
-    getBucket(
-        objectToHash, 
-        hashKeySelector
-    ) {
-        let key = hashKeySelector(objectToHash);
-        if (!isString(key))
-            key = stringifyObject(key);
-        return this.get(key);
-    }
-
-    getBuckets() {
-        return Array.from(this.values());
-    }
-
-    getKeyedBuckets() {
-        let buckets = [];
-        for(let [key,bucket] of this) {
-            bucket.key = key;
-            buckets.push(bucket);
-        }
-        return buckets;
-    }
-
-    * crossMapRow(incomingRow, hashKeySelector, mapper) {
-
-        let existingRows = this.getBucket(incomingRow, hashKeySelector);                
-
-        if (existingRows === undefined)
-            existingRows = [undefined];
-
-        for(let existingRow of existingRows) {
-
-            let mapped = mapper(existingRow, incomingRow);
-
-            if (Array.isArray(mapped)) {
-                for (let entry of mapped) 
-                    if (entry !== undefined) 
-                        yield entry;
-            }
-                        
-            else if (mapped !== undefined) 
-                yield mapped;
-
-        }
-
-    }
-    
-}
-
 function* quickSort (
     unsorted, 
     func,
@@ -2298,6 +2212,83 @@ parser.pairEqualitiesToObjectSelectors = function(func) {
 
 };
 
+class hashBuckets extends Map {
+    
+    constructor (
+        hashKeySelector,
+        distinct = false
+    ) {
+        super();
+        this.distinct = distinct;
+        this.hashKeySelector = hashKeySelector;
+    }  
+ 
+    addItems(items) {
+        for(let item of items)
+            this.addItem(item);
+        return this;
+    }
+
+    addItem(item) {
+
+        let key = this.hashKeySelector(item);
+        if (!isString(key))
+            key = stringifyObject(key);
+
+        if (this.distinct) {
+            this.set(key, [item]);
+            return this;
+        }
+
+        if(!this.has(key))
+            this.set(key, []);
+
+        this.get(key).push(item);
+
+        return this;
+
+    }
+
+    getBucket(
+        objectToHash, 
+        hashKeySelector
+    ) {
+        let key = hashKeySelector(objectToHash);
+        if (!isString(key))
+            key = stringifyObject(key);
+        return this.get(key);
+    }
+
+    getBuckets() {
+        return Array.from(this.values());
+    }
+
+    * crossMapRow(incomingRow, hashKeySelector, mapper) {
+
+        let existingRows = this.getBucket(incomingRow, hashKeySelector);                
+
+        if (existingRows === undefined)
+            existingRows = [undefined];
+
+        for(let existingRow of existingRows) {
+
+            let mapped = mapper(existingRow, incomingRow);
+
+            if (Array.isArray(mapped)) {
+                for (let entry of mapped) 
+                    if (entry !== undefined) 
+                        yield entry;
+            }
+                        
+            else if (mapped !== undefined) 
+                yield mapped;
+
+        }
+
+    }
+    
+}
+
 function* hashMerge (
     leftData, 
     rightData, 
@@ -2375,11 +2366,135 @@ function* yieldMapped (mapped) {
         yield mapped;
 }
 
-class dataset {
+class grouping {
 
-    constructor(data, groupLevel = 1) {
+    constructor(key) {
+        this.key = key;
+        this.parent = null;
+        this.children = [];
+        this.data = null;
+        this.dataIsNaked = false;
+    }
+
+    apply (tableLevelFunc) {
+
+        if (this.dataIsNaked) {
+            this.group();
+            this.apply(tableLevelFunc);
+            this.ungroup();
+            return;
+        }
+
+        if (this.data != null) {
+            this.data = (function*(data) { yield* tableLevelFunc(data); })(this.data); 
+            return;
+        }
+
+        for(let child of this.children)
+            child.apply(tableLevelFunc);
+
+    }
+
+    arrayify () {
+
+        let list = [];
+        list.key = this.key;
+
+        if (this.dataIsNaked)
+            return this.data;
+        else if (this.data != null) 
+            list.push(...this.data);
+
+        for(let child of this.children)
+            list.push(child.arrayify());
+
+        return list;
+
+    }
+
+    group (hashFunc) {
+
+        if (this.dataIsNaked) {
+            this.data = [this.data];
+            this.dataIsNaked = false;
+            console.warn(
+                'hashFunc is ignored when calling .group()' + 
+                'if data is a naked object'
+            );
+            return;
+        }
+
+        if (this.data != null) {
+            for(let [key,bucket] of new hashBuckets(hashFunc).addItems([...this.data])) {
+                let g = new grouping(key);
+                g.parent = this;
+                g.data = bucket;
+                this.data = null;
+                this.children.push(g); 
+            }
+            return;
+        }
+
+        for(let child of this.children)
+            child.group(hashFunc);
+
+
+    }
+
+    ungroup () {
+
+        if (this.children.length == 0 && this.parent == null) {
+            let nextVal = this.data.next().value;
+            if (!this.data.next().done) 
+                throw `calling ungroup on a grouping with no parent ` +
+                    `and more than one item in data is not permitted.`
+            this.data = nextVal;
+            this.dataIsNaked = true;
+            return 'there is no parent so you should never see this';
+        }
+  
+        if (this.children.length == 0) {
+            if (this.parent.data == null)
+                this.parent.data = [];
+            this.parent.data.push(...this.data);
+            return 'removeFromParent';
+        }
+        
+        for(let ch = this.children.length - 1; ch >= 0; ch--) {
+            let child = this.children[ch];
+            let decision = child.ungroup();
+            if (decision == 'removeFromParent')
+                this.children.splice(ch,1);
+        }
+
+        return 'doNotRemoveFromParent';
+
+    }
+
+}
+
+grouping.groupify = (arrayified, _parent) => {
+
+    let grp = new grouping();
+    grp.parent = _parent || null;
+    grp.key = arrayified.key || null;
+
+    for(let row of arrayified) 
+        if (Array.isArray(row)) {
+            grp.children.push(grouping.groupify(row, grp));
+        }
+        else 
+            grp.data = (function*() { yield* arrayified; })();
+
+    return grp;
+
+};
+
+class dataset extends grouping {
+
+    constructor(data) {
+        super(null);
         this.data = data;
-        this.groupLevel = groupLevel;
     }
 
     *[Symbol.iterator]() { 
@@ -2387,81 +2502,40 @@ class dataset {
     }
 
     map (func) {    
-        let _map = function* (data) {
+        this.apply(function* (data) {
             for(let row of data)
                 yield noUndefined(func(row));
-        };
-        this.data = recurse(_map, this.data, this.groupLevel);
+        });
         return this;
     }
 
-    filter (func) {    
-        let _filter = function* (data) {
+    filter (func) { 
+        this.apply(function* (data) {
             for(let row of data)
-            if(func(row))
-                yield row;
-        };
-        this.data = recurse(_filter, this.data, this.groupLevel);
+                if(func(row))
+                    yield row;
+        });
         return this;
     }
 
-    sort (func) {
-        let outerFunc = parser.parameters(func).length > 1 
-            ? data => quickSort(data, func, false)
-            : data => quickSort(data, func, true);
-        this.data = recurse(outerFunc, this.data, this.groupLevel);
+    sort (sorter) {
+        let _sorter = parser.parameters(sorter).length > 1 
+            ? data => quickSort(data, sorter, false)
+            : data => quickSort(data, sorter, true);
+        this.apply(_sorter);
         return this;
     } 
-/*
-    group (func) {
-        let outerFunc = data => 
-            new hashBuckets(func)
-            .addItems(data)
-            .getBuckets();
-        this.data = recurse(outerFunc, this.data, this.groupLevel);
-        this.groupLevel++;
-        return this;
-    }
-*/
 
     group (func) {
-        let outerFunc = data => 
-            new hashBuckets(func)
-            .addItems(data)
-            .getKeyedBuckets();
-        this.data = recurse(outerFunc, this.data, this.groupLevel);
-        this.groupLevel++;
+        super.group(func);
         return this;
     }
 
-    ungroup (func) {
-
-        if (!func) 
-            func = x => x;
-
-        if (this.groupLevel == 1) {
-            let counter = 0;
-            for (let item of this.data) {
-                if (++counter > 1)
-                    throw   'Ungrouping to level 0 is possible, but ' +
-                            'there can only be one item in the dataset.';
-                this.data = item;
-            }
-            this.groupLevel--;
-            return this;
-        }
-
-        let outerFunc = function* (data) {
-            for (let item of data)
-            for (let nested of item)
-                yield func(nested);
-        };
-
-        // stop early becuase you want one level above base records
-        this.data = recurse(outerFunc, this.data, this.groupLevel - 1);
-        this.groupLevel--;
+    ungroup (mapper) {
+        if (mapper)
+            this.map(mapper);
+        super.ungroup();
         return this;
-
     }
 
     reduce (obj, ungroup = true) {
@@ -2469,45 +2543,38 @@ class dataset {
         let isNaked = Object.keys(obj).length == 0;
 
         // wrap result in array to bring back to original nesting level
-        let outerFunc = data => {
+        this.apply(data => {
             let agg = {};
             if (isNaked)
                 return [obj(data)];
-            for(let [key,reducer] of Object.entries(obj)) {
+            for(let [key,reducer] of Object.entries(obj)) 
                 agg[key] = reducer(data);
-            }
             return [agg]; 
-        };
+        });
 
-        this.data = recurse(outerFunc, this.data, this.groupLevel);
-
-        if (ungroup)
+        if (ungroup) 
             this.ungroup();
 
         return this;
 
     }
 
-    distinct (func, sorter) {
+    distinct (hashKeySelector, sorter) {
 
-        func = func || (x => x);
+        hashKeySelector = hashKeySelector || (x => x);
         
-        if (sorter) sorter = 
-            parser.parameters(sorter).length > 1 
-            ? data => quickSort(data, func, false)
-            : data => quickSort(data, func, true);
-        else 
-            sorter = data => data;
+        let getFirstBucketItem = sorter
+            ? (bucket) => new dataset(bucket).sort(sorter)[0]
+            : (bucket) => bucket[0];
+            
 
-        let outerFunc = data => 
-            new hashBuckets(func)
-            .addItems(data)
-            .getBuckets()
-            .map(bucket => {
-                return [...sorter(bucket)][0]
-            });
+        this.apply(data => 
+            new hashBuckets(hashKeySelector)
+                .addItems(data)
+                .getBuckets()
+                .map(getFirstBucketItem)
+        );
 
-        this.data = recurse(outerFunc, this.data, this.groupLevel);
         return this;
 
     }
@@ -2557,7 +2624,7 @@ class dataset {
 
         // terminations
 
-            let outerFunc = leftHasher && rightHasher
+            let mergeFunc = leftHasher && rightHasher
                 ? data => [...hashMerge( 
                         data, rightData, 
                         matcher, mapper, 
@@ -2569,7 +2636,7 @@ class dataset {
                         matcher, mapper
                     )];
 
-            this.data = recurse(outerFunc, this.data, this.groupLevel); 
+            this.apply(mergeFunc); 
             return this;
 
     }
@@ -2658,62 +2725,16 @@ class dataset {
 
     }
 
-    get (func) {
-
-        if (!isIterable(this.data)) {
-            if (func)
-                this.data = func(this.data);
-            return this.data;
-        }
-        let arr = recurseToArray(
-            func || function(x) { return x }, 
-            this.data,
-            this.groupLevel
-        );
-        this.data = arr;
-        return arr;
+    get (mapper = null) {
+        if (mapper)
+            this.map(mapper);
+        return this.arrayify();
     }
 
     toJsonString(func) {
-        let dataJson = JSON.stringify(this.get(func));
-        return `{"data":${dataJson},"groupLevel":${this.groupLevel}}`;
+        let getted = this.get(func);
+        return JSON.stringify(getted);
     }
-
-}
-
-function* recurse (func, data, levelCountdown) {
-
-    if (levelCountdown === 0)
-        return func([data])[0];
-
-    if (levelCountdown > 1) { // data is nested groups
-        for (let item of data) 
-            yield recurse(func, item, levelCountdown - 1);
-        return;
-    }
-
-    yield* func(data); // data is base records
-
-}
-
-function recurseToArray (func, data, levelCountdown) {
-
-    if (levelCountdown === 0)
-        return func([data])[0];
-
-    let list = [];
-
-    for(let item of data)
-        list.push(
-            levelCountdown > 1          
-            ? recurseToArray(func, item, levelCountdown - 1)
-            : noUndefined(func(item))
-        );
-
-    if (data.key) 
-        list.key = data.key;
-    
-    return list;    
 
 }
 
@@ -2725,20 +2746,19 @@ function _(obj) {
 
 _.fromJson = function(json) {
 
-    let ds = new dataset();
+    let groupify = (arrayified) => {
+        let parsed = isString(arrayified) 
+            ? JSON.parse(arrayified) 
+            : arrayified;
+        let groupified = grouping.groupify(parsed);
+        let ds = new dataset();
+        ds.data = groupified.data;
+        return ds;
+    };
 
-    if (json.constructor.name == 'Response') 
-        return json.json().then(parsed => {
-            ds.data = parsed.data;
-            ds.groupLevel = parsed.groupLevel;
-            return ds;
-        });
-
-    let parsed = isString(json) ? JSON.parse(json) : json;
-    ds.data = parsed.data;
-    ds.groupLevel = parsed.groupLevel;
-
-    return ds;
+    return json.constructor.name == 'Response' 
+        ? json.json().then(groupify)
+        : groupify(json);
 
 };
 
