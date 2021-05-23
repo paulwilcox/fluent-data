e7edd090ba07892baae5720ad717a39ab01c2d08
paulwilcox <t78t78@gmail.com>
2021-05-23 11:32:51 -0700

    Added test for new reduce features

--- test/reduce.js
+++ test/reduce.js
@@ -9,13 +9,14 @@ async function test () {
             speed: $$.avg(o => o.speed),
             rating: $$.avg(o => o.rating),
             speed_cor: $$.cor(o => [o.speed, o.rating]),
+            nn: (acc,next) => acc += 1,
+            ['nn.seed']: 100,
             n: $$.count(o => o.id)
         })
         .get();
 
-    if(results.n != 12) throw `
-        results.n does not equal 12
-    `;
+    if (results.n != 12)     throw `results.n does not equal 12`;
+    if (results.nn != 112)   throw `results.nn does not equal 112`;
 
     results = 
         $$(data.orders)
